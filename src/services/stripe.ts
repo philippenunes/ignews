532b1f2e4883372019a9dfc07ba1e39b33fd322a
Philippe Nunes <philippe.silva@mprj.mp.br>
2025-08-16 20:42:07 -0300

    remove unnecessary validation

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,12 +1,8 @@
 import Stripe from 'stripe'
 import { version } from '../../package.json'
 
-if (!process.env.STRIPE_API_KEY) {
-    throw new Error("❌ Variável STRIPE_API_KEY não definida no .env.local");
-}
-
 export const stripe = new Stripe(
-    process.env.STRIPE_API_KEY,
+    process.env.STRIPE_API_KEY!,
     {
         apiVersion: '2025-07-30.basil',
         appInfo: {
