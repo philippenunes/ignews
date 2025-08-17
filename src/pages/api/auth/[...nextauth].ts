d20135a23838832167d1967c2f38ac0fa1d3f23e
Philippe Nunes <philippe.silva@mprj.mp.br>
2025-08-17 13:55:58 -0300

    add `user email` to the database when logged in

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,33 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth, { AuthOptions, Account, Profile, User } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { db } from "../../../services/firebase";
+import { collection, addDoc } from "firebase/firestore";
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-      authorization: {
-        params: { scope: "read:user" },
-      },
-    })
+      authorization: { params: { scope: "read:user" } },
+    }),
   ],
-}
+  callbacks: {
+    async signIn({ user }): Promise<boolean> {
+      try {
+        await addDoc(
+          collection(db, "users"),
+          {
+            email: user.email,
+            createdAt: new Date()
+          }
+        );
+      } catch {
+        return false;
+      }
 
-export default NextAuth(authOptions)
+      return true;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
