efdf9bfcc17d90f5edfd468fc72893c771e978f5
Philippe Nunes <philippe.silva@mprj.mp.br>
2025-08-16 21:09:26 -0300

    config authentication using `next-auth` with `github-provider`

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,28 +1,28 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
-//import { signIn, signOut, useSession } from 'next-auth/client';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
-export function SignInButton() {
-  // const [session] = useSession();
-  const isLoggedIn = true;
 
-  return isLoggedIn ? (
+export function SignInButton() {
+  const { data: session } = useSession();
+
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
-      onClick={() => { }}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Philipe Nunes
+      {session?.user?.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
-      onClick={() => { }}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
