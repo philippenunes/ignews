f5e0ef4c319d17a5c80a8a905e71b6b77bc18469
Philippe Nunes <philippe.silva@mprj.mp.br>
2025-09-02 22:39:32 -0300

    feat: `post` component

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -2,6 +2,7 @@ import { signIn, useSession } from 'next-auth/react';
 import styles from './styles.module.scss';
 import { api } from '@/src/services/api';
 import { getStripeJs } from '@/src/services/stripe.-js';
+import { useRouter } from 'next/router';
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -9,10 +10,16 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session } = useSession();
+  const router = useRouter()
 
   async function handleSubscribe() {
     if (!session) {
       signIn('github');
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts')
       return;
     }
 
